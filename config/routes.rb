7debcdbeafb86450305ca338978abd2f0b63a2ac
Nicolas Esnis <nicolas.esnis@gmail.com>
2017-09-29 16:01:23 +0200

    optional 1 done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+  get 'articles', to: 'articles#index', as: :articles
+
+  get 'articles/new', to: 'articles#new', as: :article_new
+  post 'articles', to: 'articles#create'
+
+  get 'articles/:id/edit', to: 'articles#edit', as: :article_edit
+  patch 'articles/:id', to: 'articles#update'
+
+  get 'articles/:id', to: 'articles#show', as: :article
+
+  delete 'articles/:id', to: 'articles#destroy', as: :article_destroy
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end
